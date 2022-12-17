8da6d4fbf199fdbb95ef3512f8a99a38096a6eed
Emi Mata <38539473+iamEmiMata@users.noreply.github.com>
2022-12-17 17:11:11 -0400

    files

--- python/sequence.py
+++ python/sequence.py
@@ -6,22 +6,6 @@ Valid Parentheses
 (), [], {}
 
 '''
-# class solution:
-# 	def isValid(self, sequence):
-# 		while True:
-# 			if '()' in sequence:
-# 				sequence = sequence.replace('()', '')
-# 			elif '[]' in sequence:
-# 				sequence = sequence.replace('[]', '')
-# 			elif '{}' in sequence:
-# 				sequence = sequence.replace('{}', '')
-# 			else :
-# 				return not sequence
-
-# sequence = input('>> ')
-# print(f'Is {sequence} valid ? : {solution().isValid(sequence)}')
-
-
 def valid(sequence):
 	while True: 
 		if '()' in sequence:
